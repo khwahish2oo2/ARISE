696e1b9271efcf4b454ec452b0ce320dc326bc03
vaishnavi <vaish.ldh.123@gmail.com>
2023-12-07 22:20:05 +0530

    done changes for integration

--- public/main.py
+++ public/main.py
@@ -12,28 +12,46 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-cred = credentials.Certificate("login-database-a039a-firebase-adminsdk-nl21h-077c59e4c6.json")
-firebase_app = initialize_app(cred, {"databaseURL": "https://login-database-a039a-default-rtdb.firebaseio.com"})
+# cred = credentials.Certificate("login-database-a039a-firebase-adminsdk-nl21h-077c59e4c6.json")
+# firebase_app = initialize_app(cred, {"databaseURL": "https://login-database-a039a-default-rtdb.firebaseio.com"})
+
 
 @app.get("/", response_class=HTMLResponse)
 async def hello(request: Request):
    return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
    return templates.TemplateResponse("login.html", {"request": request})
+
 
 @app.get("/reset_password", response_class=HTMLResponse)
 async def reset_password(request: Request):
    return templates.TemplateResponse("reset_password.html", {"request": request})
 
 
-
-
-    
-    
-
 @app.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/OurServices", response_class=HTMLResponse)
+async def OurServices(request: Request):
+   return templates.TemplateResponse("home-entrepreneur.html", {"request": request})
+
+@app.get("/YourProfile", response_class=HTMLResponse)
+async def YourProfile(request: Request):
+   return templates.TemplateResponse("profile.html", {"request": request})
+
+@app.get("/Upload", response_class=HTMLResponse)
+async def Upload(request: Request):
+   return templates.TemplateResponse("Upload_doc.html", {"request": request})
+
+@app.get("/question", response_class=HTMLResponse)
+async def question(request: Request):
+   return templates.TemplateResponse("question.html", {"request": request})
+
+
+
+
 
