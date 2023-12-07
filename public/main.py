c4eed750821187892dd40ed9bb5c82766743e0b6
vaishnavi <vaish.ldh.123@gmail.com>
2023-12-07 19:51:32 +0530

    changes

--- public/main.py
+++ public/main.py
@@ -1,17 +1,3 @@
-# from fastapi import FastAPI
-# app = FastAPI()
-# @app.get("/")
-# async def index():
-#    return {"message": "Hello World"}
-
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# from fastapi import FastAPI, Request
-# app = FastAPI()
-# templates = Jinja2Templates(directory="Templates")
-# @app.get("/", response_class=HTMLResponse)
-# async def hello(request: Request):
-#    return templates.TemplateResponse("index.html", {"request": request})
 
 from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import HTMLResponse
@@ -41,6 +27,8 @@ async def login(request: Request):
 async def reset_password(request: Request):
    return templates.TemplateResponse("reset_password.html", {"request": request})
 
+
+
 @app.post("/login")
 async def login(
     email: str = Form(...),
@@ -58,34 +46,10 @@ async def login(
 
     except auth.AuthError as e:
         raise HTTPException(status_code=401, detail=f"Login failed: {e}")
+    
+    
 
 @app.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
    return templates.TemplateResponse("register.html", {"request": request})
 
-# @app.post("/submit/")
-# async def submit(
-#     request: Request,
-#     Email: str = Form(...),
-#     Password: str = Form(...),
-#     full_name: str = Form(...),
-#     first_school: str = Form(...),
-# ):
-#     try:
-#         # Create user in Firebase Authentication
-#         user = auth.create_user(
-#             email=Email,
-#             password=Password,
-#         )
-
-#         # Store additional user information in Firebase Realtime Database
-#         user_data = {
-#             "fullName": full_name,
-#             "firstSchool": first_school,
-#         }
-#         db.reference(f"UsersAuthList/{user.uid}").set(user_data)
-
-#         return templates.TemplateResponse("login.html", {"request": request})
-
-#     except auth.AuthError as e:
-#         raise HTTPException(status_code=400, detail=f"Registration failed: {e}")
